8ad80e0efb8573dbb789738e716aaeb7722a2c5f
KatyaChe <77936323+KatyaChe@users.noreply.github.com>
2021-05-27 23:36:18 +0300

    choice.js

---
+++ choice.js
@@ -0,0 +1,8 @@
+function color() {
+    if (document.getElementById("colors").value == "yell") 
+        document.getElementById("result").style.backgroundColor="#FFFF00";
+        if (document.getElementById("colors").value == "green") 
+        document.getElementById("result").style.backgroundColor="green";
+        if (document.getElementById("colors").value == "orange") 
+        document.getElementById("result").style.backgroundColor="orange";
+ }
